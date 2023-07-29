06ddb2be696a497c3b390baf3e488187cf69166c
abigailmgarcia <abigailmg31@gmail.com>
2023-07-29 01:18:23 -0500

    exported correctly

---
+++ Develop/server.js
@@ -0,0 +1,36 @@
+// initalize express and create port
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+// const apiRoutes = require ('./apiRoutes');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// app.use('/api', api);
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for to notes page from main page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+// Wildcard route to direct users back to the landing page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
+// Start the server and listen for incoming requests
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
+
+module.exports = app;
