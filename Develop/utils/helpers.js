566b8834df20ecb1ee02fd7bf072ab961765de67
abigailmgarcia <abigailmg31@gmail.com>
2023-07-29 01:36:42 -0500

    write to file

--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -1,2 +1,16 @@
 const fs = require('fs');
 const util = require('util');
+//promise version of fs.read
+const readFromFile = util.promisify(fs.readFile);
+
+//write data to JSON and give content
+@param {string}
+@param {object}
+@returns {void}
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null,4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+        );
+
+        
