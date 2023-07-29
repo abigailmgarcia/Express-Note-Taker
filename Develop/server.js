c78150a318257b7305cec69dbbaa6d0caf2b8e2f
abigailmgarcia <abigailmg31@gmail.com>
2023-07-29 02:41:16 -0500

    added routes folder

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const notes = require('./routes/index')
 // const apiRoutes = require ('./apiRoutes');
 
 const PORT = process.env.PORT || 3000;
@@ -13,9 +14,10 @@ app.use(express.urlencoded({extended: true}));
 // app.use('/api', api);
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+app.use('/api', notes);
 
 //GET route for to notes page from main page
 app.get('/notes', (req, res) => {
--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -4,13 +4,30 @@ const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
 
 //write data to JSON and give content
-@param {string}
-@param {object}
-@returns {void}
+// @param {string} destination 
+// @param {object} content
+// @returns {void}
 
 const writeToFile = (destination, content) =>
     fs.writeFile(destination, JSON.stringify(content, null,4), (err) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
         );
 
-        
+    //function read data from file and append
+        // @param {string} file
+        // @param {object} content
+        // @returns {void}
+
+    const readAndAppend = (content, file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(content);
+                writeToFile(file, parsedData);
+            }
+        });
+    };
+
+    module.exports = { readFromFile, writeToFile, readAndAppend};
